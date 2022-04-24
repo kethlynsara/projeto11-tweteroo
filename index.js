7337c5dbc931a7543007d656f2194d4badd90db3
Kethlyn <kethlynsaraa@gmail.com>
2022-04-24 00:46:20 -0300

    GET Tweets: últimos 10 tweets

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
@@ -9,22 +9,25 @@ let users = [];
 let tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body;
-    const signUp = {username, avatar};
-    users.push(signUp);
-    res.statusMessage = "OK";
-    res.status(200).end();
+  const { username, avatar } = req.body;
+  const signUp = { username, avatar };
+  users.push(signUp);
+  res.statusMessage = "OK";
+  res.status(200).end();
 });
 
 app.post("/tweets", (req, res) => {
-    const {username, tweet} = req.body;
-    const data = {username, tweet};
-    tweets.push(data);
-    res.status(200).send("OK");
+  const { username, tweet } = req.body;
+  const data = { username, tweet };
+  const usuario = users.find((user) => user.username === data.username);
+  const tweetNovo = {...data, avatar: usuario.avatar};
+  tweets.push(tweetNovo);
+  res.status(200).send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets);
+  const tweets10 = tweets.slice(-10);
+  res.send(tweets10);
 });
 
 app.listen(5000);
